ef6a3675a45ec4f3625b74ebd2fb2007fab44124
Awais Ashraf <mhawais504@gmail.com>
2024-07-13 13:39:31 +0500

    initial commit with some design

---
+++ curdrealtimeadmin/src/main/java/com/example/curdrealtimeadmin/DataClass.kt
@@ -0,0 +1,13 @@
+package com.example.curdrealtimeadmin
+
+data class DataClass(
+    var ownerName:String?=null,
+    var vehicleBrand:String?=null,
+    var vehicleRto:String?=null,
+    var vehicleNumber:String?=null
+
+
+)
+{
+
+}
